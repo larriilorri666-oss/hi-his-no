f73cb99c4004efa3c12abdde07963c3fc53cf7c5
larriilorri666-oss <larri.i.lorri.666@gmail.com>
2026-09-25 22:38:48 +0300

    Create аpp.py

---
+++ аpp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("🛡️ Проверка доступа на сайт")
+
+name = st.text_input("Как тебя зовут?")
+age = st.slider("Сколько тебе лет?", min_value=0, max_value=100, value=18)
+
+if st.button("Проверить доступ"):
+    if age == 18:
+        st.balloon()
+        st.success(f"Ого, {name}, тебе как раз 18! Добро пожаловать во взрослую жизнь! 🥳")
+    elif age > 18:
+        st.success(f"Привет, {name}! Доступ разрешен. 👍")
+    else:
+        st.error(f"Извини, {name}, но тебе ещё рано сюда. 🛑")
